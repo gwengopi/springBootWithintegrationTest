c3ee9e6cc9935dc41c91800b6002d52dd4f13faf
gwengopi <gwengopi.gg@gmail.com>
2023-11-01 09:07:07 +0530

    BookDTO file property names are changed and mapper class is modified according to that to map the fields

--- src/main/java/home/java/learn/spring/mvc/mapper/BookMapper.java
+++ src/main/java/home/java/learn/spring/mvc/mapper/BookMapper.java
@@ -3,11 +3,19 @@ package home.java.learn.spring.mvc.mapper;
 import home.java.learn.spring.mvc.dto.BookDTO;
 import home.java.learn.spring.mvc.model.Book;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+import org.springframework.stereotype.Component;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface BookMapper {
 
+    BookMapper MAPPER = Mappers.getMapper(BookMapper.class);
+
+    @Mapping(source = "title", target = "bookTitle")
+    @Mapping(source = "author", target = "bookAuthor")
+    @Mapping(source = "year", target = "publishedYear")
     BookDTO toDTO(Book book);
-    Book toEntity(BookDTO bookDTO);
+    Book toEntity(home.java.learn.spring.mvc.dto.BookDTO bookDTO);
 
 }
