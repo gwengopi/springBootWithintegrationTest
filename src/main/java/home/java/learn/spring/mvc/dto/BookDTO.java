3780433274040fa5105e70c6a148ee24675c13a8
gwengopi <gwengopi.gg@gmail.com>
2023-11-03 13:57:19 +0530

    added validation dependencies and introduced validation in DTO class

--- src/main/java/home/java/learn/spring/mvc/dto/BookDTO.java
+++ src/main/java/home/java/learn/spring/mvc/dto/BookDTO.java
@@ -1,6 +1,9 @@
 package home.java.learn.spring.mvc.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +17,8 @@ public class BookDTO {
 
     @JsonIgnore
     private Long id;
+    @NotNull
+    @NotEmpty()
     private String bookTitle;
     private String bookAuthor;
     private int publishedYear;
