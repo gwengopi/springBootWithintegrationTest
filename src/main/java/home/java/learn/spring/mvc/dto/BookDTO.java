4419843c0fe788a3216e92a817cd71715c40ba59
gwengopi <gwengopi.gg@gmail.com>
2023-11-09 09:49:52 +0530

    ToString annotation to print the object details

--- src/main/java/home/java/learn/spring/mvc/dto/BookDTO.java
+++ src/main/java/home/java/learn/spring/mvc/dto/BookDTO.java
@@ -4,15 +4,13 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class BookDTO {
 
     @JsonIgnore
