5266f53fee61d1e98a5a242ce63270b63abad8ad
gwengopi <gwengopi.gg@gmail.com>
2023-11-11 21:18:07 +0530

    User service with basic outline - todo correct all api implementation

--- src/main/java/home/java/learn/spring/mvc/user/service/UserServiceImpl.java
+++ src/main/java/home/java/learn/spring/mvc/user/service/UserServiceImpl.java
@@ -2,23 +2,27 @@ package home.java.learn.spring.mvc.user.service;
 
 import home.java.learn.spring.mvc.user.UserRepository;
 import home.java.learn.spring.mvc.user.dto.UserDTO;
+import home.java.learn.spring.mvc.user.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
-    // For simplicity, I'm using an in-memory list to store users.
-/*    private final List<UserDTO> users;
+
+    @Autowired
+    private UserMapper userMapper;
+    private final List<UserDTO> users;
 
     public UserServiceImpl(List<UserDTO> users) {
         this.users = users;
-    }*/
+    }
 
     @Override
     public UserDTO registerUser(UserDTO user) {
@@ -28,18 +32,20 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<UserDTO> getAllUsers() {
-        return users;
+        return null;
     }
 
     @Override
     public Optional<UserDTO> getUserById(Long userId) {
-        return users.stream()
-                .filter(user -> user.getId().equals(userId))
-                .findFirst();
+//        return users.stream().map(userMapper::toUser).collect(Collectors.toList()).s
+//                .filter(user -> user.getId().equals(userId))
+//                .findFirst();
+        return Optional.empty();
     }
 
     @Override
     public void deleteUser(Long userId) {
-        users.removeIf(user -> user.getId().equals(userId));
+//        users.removeIf(user -> user.getId().equals(userId));
+
     }
 }
