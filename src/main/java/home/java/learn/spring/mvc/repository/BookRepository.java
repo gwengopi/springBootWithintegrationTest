7d46f2c0c5e19ce33fd70db0bfd7484adaa68fd0
gwengopi <gwengopi.gg@gmail.com>
2023-11-13 19:12:48 +0530

    Added base path for spring data rest and implemented RestResource annotation to expose a new method

--- src/main/java/home/java/learn/spring/mvc/repository/BookRepository.java
+++ src/main/java/home/java/learn/spring/mvc/repository/BookRepository.java
@@ -2,9 +2,23 @@ package home.java.learn.spring.mvc.repository;
 
 
 import home.java.learn.spring.mvc.model.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+@RepositoryRestResource(collectionResourceRel = "book",path = "book")
 public interface BookRepository extends JpaRepository<Book, Long> {
-    // Add custom query methods if needed
+
+//    @RestResource(path = "title", rel = "title")
+    public List<Book> findByTitle(@Param("title") String title, Pageable p);
+
+
 }
 
